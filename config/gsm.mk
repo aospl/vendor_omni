317169cdaf8adeb6b5a581954433d39525934f81
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-11 16:13:58 +0900

    change apns-conf.xml and add spn-conf.xml

--- config/gsm.mk
+++ config/gsm.mk
@@ -1,6 +1,7 @@
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/omni/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
